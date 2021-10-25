63c9ea5f65dd2a4c9862a1cba7240f1cfb07afa2
rodrigomato00 <3293@holbertonschool.com>
2021-10-25 14:12:30 +0000

    mas a rreglos de betty

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,65 +1,53 @@
 #include "sort.h"
 
 /**
- *nodo_intercambiador -
- *@c:
- *@m:
+ *nodo_intercambiador - nodo intercambiador
+ *@c: c
+ *@m: m
  *Return: void
  */
 void nodo_intercambiador(listint_t **c, unsigned char m)
 {
-	listint_t *temporal0;
-	listint_t *temporal1;
+	listint_t *temporal0, *temporal1;
 
 	temporal0 = NULL;
 	temporal1 = NULL;
-
-        if (m == 0)
+	if (m == 0)
 	{
-                temporal0 = (*c)->prev;
-
+		temporal0 = (*c)->prev;
 		if (temporal0)
-			temporal1 = temporal0->prev;
-
+			temporal1 = temporal0->prev
 		if ((*c)->next)
 			(*c)->next->prev = temporal0;
-
 		temporal0->next = (*c)->next;
 		temporal0->prev = *c;
 		(*c)->next = temporal0;
 		(*c)->prev = temporal1;
-
 		if (temporal1)
 			temporal1->next = *c;
-
 		*c = temporal0;
 	}
-
 	else if (m == 1)
 	{
-
 		temporal0 = (*c)->next;
-
 		if (temporal0)
 			temporal1 = temporal0->next;
-
 		if ((*c)->prev)
 			(*c)->prev->next = temporal0;
 		temporal0->prev = (*c)->prev;
 		temporal0->next = *c;
 		(*c)->prev = temporal0;
 		(*c)->next = temporal1;
-
 		if (temporal1)
 			temporal1->prev = *c;
-
 		*c = temporal0;
 	}
 }
 
 /**
- *cocktail_sort_list -
- *@list:
+ *cocktail_sort_list - sorts a doubly linked list of integers in ascending
+ *order using the Cocktail shaker sort algorithm
+ *@list: list
  *Return:void
  */
 void cocktail_sort_list(listint_t **list)
@@ -69,17 +57,14 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *c2;
 
 	j = 1;
-
 	if (!list || !(*list) || !((*list)->next))
 		return;
 
 	while (j)
 	{
-
 		j = 0;
 		for (c = *list; c; c = c->next)
 		{
-
 			if (c->prev && (c->prev->n > c->n))
 			{
 				j = 1;
@@ -93,7 +78,6 @@ void cocktail_sort_list(listint_t **list)
 			if (c->next == NULL)
 				c2 = c;
 		}
-
 		for (; c2; c2 = c2->prev)
 		{
 
